3421470e6d553a5e210e472323c9158c78c0640d
Jorge Cortés <guardate1@gmail.com>
2017-12-29 15:35:39 +0100

    Página de producto y ruta lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ import {
 const app_routes: Routes = [
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'item', component: ItemComponent },
+    { path: 'producto/:id', component: ItemComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,9 +19,13 @@ this.cargando = true;
 this.http.get('https://paginaweb-bea4b.firebaseio.com/productos_idx.json')
 .subscribe( res => {
 
-console.log( res.json()  );
+//console.log( res.json()  );
+
+//setTimeout( ()=>{
 this.cargando = false;
 this.productos = res.json();
+
+//},1500)
 });
 
   }
