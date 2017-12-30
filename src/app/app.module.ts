1beadbc03bec18cadba33588c003e73b65d853f8
Jorge Cortés <guardate1@gmail.com>
2017-12-30 13:28:00 +0100

    Página de busqueda creada - no funciona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ItemComponent } from './components/item/item.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
@@ -28,7 +29,8 @@ import { ItemComponent } from './components/item/item.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
+    SearchComponent
   ],
   imports: [ 
     BrowserModule,
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,5 +1,7 @@
+import { ProductosService } from '../../services/productos.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-item',
@@ -8,12 +10,25 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
-  constructor( private route:ActivatedRoute) { 
+  producto:any = undefined;
+  cod:string = undefined;
+
+  constructor( private route:ActivatedRoute,
+               private _ps: ProductosService) { 
 
     route.params.subscribe( parametros=>{
-      
-console.log( parametros );
-console.log( parametros['id']  );
+
+// console.log( parametros );
+// console.log( parametros['id']  );
+
+_ps.cargar_producto( parametros['id'] )
+.subscribe( res => {
+
+  this.cod = parametros['id'];
+this.producto = res.json();
+console.log( this.producto );
+
+});
 
     })
   }
